3bbd0d9e32f15111c5447d1a49485d9b208bbbbe
liuqingchao@fenbi.com <liuqingchao@fenbi.com>
2022-03-23 23:16:35 +0800

    双指针代码

--- src/main/java/unit9_双指针和滑动窗口/ch2_2最长子串专题/LengthOfLongestSubstring.java
+++ src/main/java/unit9_双指针和滑动窗口/ch2_2最长子串专题/LengthOfLongestSubstring.java
@@ -1,14 +1,15 @@
 package unit9_双指针和滑动窗口.ch2_2最长子串专题;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LengthOfLongestSubstring {
     public static void main(String[] args) {
 
-//        String s = "abcabcbb";
+        String s = "abcabcbb";
 //        String s = "au";
-        String s = " ";
+//        String s = " ";
         System.out.println(lengthOfLongestSubstring(s));
 
     }
@@ -17,24 +18,38 @@ public class LengthOfLongestSubstring {
         if (s.length() <= 1) {
             return s.length();
         }
-        Set<Character> curSet = new HashSet<>();
-        int right = 0, max = 0;
-        int n=s.length();
-        for (int left = 0; left < n; ++left) {
+        // 集合中的元素一定不重复，适合记录窗口内每个字符是否出现过，
+        Set<Character> set = new HashSet<Character>();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int right = -1, max = 0;
+        for (int left = 0; left < s.length(); left++) {
+            // 左指针向右移动时移除前一个位置的字符，因此要排除left=0的情况
             if (left != 0) {
-                curSet.remove(s.charAt(left - 1));
+                set.remove(s.charAt(left - 1));
             }
-            while (right < n - 1 && curSet.contains(s.charAt(right ))) {
-                curSet.add(s.charAt(right ));
+            while (right + 1 < s.length() && !set.contains(s.charAt(right + 1))) {
+                // 不断地移动右指针
+                set.add(s.charAt(right + 1));
                 right++;
             }
             max = Math.max(max, right - left + 1);
-
         }
-
         return max;
 
     }
 
-
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)) + 1);
+            }
+            map.put(s.charAt(right), right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
 }
