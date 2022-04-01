0d36baced4e6fa20c2c0b7d62160ad0feb704716
liuqingchao@fenbi.com <liuqingchao@fenbi.com>
2022-04-01 21:56:44 +0800

    重命名包

---
+++ src/main/java/unit2_数组/二维数组/part1_basic/Juzhen.java
@@ -0,0 +1,19 @@
+package unit2_数组.二维数组.part1_basic;
+
+public class Juzhen {
+    public static void main(String args[]) {
+        int a[][] = new int[10][10];
+
+        //取出9个数组，a[1],a[2]....a[9]
+        for (int i = 1; i < a.length; i++) {
+            //    a[i]=new int [i+1];    该数组的空间(元素数目）已经确定，不需要类似的语句进行确定
+
+            //a[1]中有a[1][0],a[1][1]....a[1][10],为a[1][1].....a[1][9]赋值，其余为空，以此类推
+            for (int j = 1; j < a[i].length; j++) {
+                a[i][j] = i * j;
+                System.out.print(i + "*" + j + "=" + a[i][j] + "\t");   //"\t"为空格键
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/unit2_数组/二维数组/part2_hot_problem/topic_2_2_螺旋打印/SpiralOrder.java
@@ -0,0 +1,46 @@
+package unit2_数组.二维数组.part2_hot_problem.topic_2_2_螺旋打印;
+
+import java.util.ArrayList;
+
+/**
+ * 【1】螺旋矩阵打印
+ */
+public class SpiralOrder {
+
+    public static void main(String[] args) {
+        int intA[][]={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(spiralOrder(intA));
+    }
+
+    public static ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res=new ArrayList<>();
+        if(matrix.length==0){
+            return res;
+        }
+        //定义四个坐标，表示每次循环的四个位置
+        int top=0,bottom=matrix.length-1;
+        int left=0,right=matrix[0].length-1;
+        while(top<(matrix.length+1)/2 &&left<(matrix[0].length+1)/2){
+            for(int i=left;i<=right;i++){
+                res.add(matrix[top][i]);
+            }
+
+            for(int i=top+1;i<=bottom;i++){
+                res.add(matrix[i][right]);
+            }
+
+            for(int i= right-1;i>=left&& top!=bottom;i--){
+                res.add(matrix[bottom][i]);
+            }
+            for(int i=bottom-1;i>=(top+1)&&left!=right;i--){
+                res.add(matrix[i][left]);
+            }
+            top++;
+            bottom--;
+            left++;
+            right--;
+
+        }
+        return res;
+    }
+}
