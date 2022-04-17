d708430c692e2c7712ff0a882b84c00259689fc2
lihao <lihao890>
2022-04-17 20:36:01 +0800

    提交第一次作业

--- src/main/java/homework/task1_array/Q2_ArrayHomeWork.java
+++ src/main/java/homework/task1_array/Q2_ArrayHomeWork.java
@@ -14,18 +14,29 @@ public class Q2_ArrayHomeWork {
      * 1.时间复杂度不能高于O(n)，空间复杂度为O（1）级别，只允许操作原始数组，不得创建新数组
      * 2.在下面removeAllDuplicate()中实现你的代码，其他代码不可以修改
      * <p>
-     * 提示：双指针思想再拓展一下
+     *   使用快慢指针的思想
+     *   移动规则： 快慢指针都从arr[0]开始向右移动，定义一个是否移动慢指针slow的变量isMove，
+     *   快指针一直向右移动，如果快指针当前位置的元素和慢指针的元素相等，就把isMove设置为false，
+     *   当快指针移动到与慢指针元素不相等的位置时，如果isMove为true,就把慢指针向右移动一位，再把快指针的元素赋值给慢指针
+     * </p?
      */
 
     /**
      * @param arr 待处理的数组
      */
     public static void removeAllDuplicate(int[] arr) {
-        //todo  这里实现你的算法
+        // 定义一个慢指针、一个快指针
         int slow = 0, fast = 1;
+        /*
+         * 定义一个是否移动慢指针slow的变量
+         */
         boolean isMove = true;
         while (fast < arr.length) {
             if (arr[slow] == arr[fast]) {
+                /*
+                 * 如果快指针在移动过程中有和慢指针相等的元素，就把isMove设置为false，
+                 * 表示后面快指针移动到与慢指针元素不同的位置时需要移动慢指针
+                 */
                 isMove = false;
             } else {
                 if (isMove) {
