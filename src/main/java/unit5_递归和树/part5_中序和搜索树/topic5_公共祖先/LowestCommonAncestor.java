1d654decca008fe730f9c88c2a44c4c175c4a99a
liuqingchao@fenbi.com <liuqingchao@fenbi.com>
2022-05-04 20:59:53 +0800

    搜索树

--- src/main/java/unit5_递归和树/part5_中序和搜索树/topic5_公共祖先/LowestCommonAncestor.java
+++ src/main/java/unit5_递归和树/part5_中序和搜索树/topic5_公共祖先/LowestCommonAncestor.java
@@ -18,7 +18,27 @@ public class LowestCommonAncestor {
         System.out.println(node.val);
     }
 
+
+    /**
+     * 递归实现
+     */
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //如果小于等于0，说明p和q位于root的两侧，直接返回即可
+        if ((root.val - p.val) * (root.val - q.val) <= 0)
+            return root;
+        //否则，p和q位于root的同一侧，就继续往下找
+        return lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
+    }
+
+    /**
+     * 迭代实现
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         //如果根节点和p,q的差相乘是正数，说明这两个差值要么都是正数要么都是负数，也就是说
         //他们肯定都位于根节点的同一侧，就继续往下找
         while ((root.val - p.val) * (root.val - q.val) > 0)
